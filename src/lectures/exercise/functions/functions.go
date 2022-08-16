aa7d3128444c10b6c01096f43add6e0f94d0f5c3
Zetta Eric Julianto <algonacci1@gmail.com>
2022-08-16 09:12:12 +0700

    learnt about function

--- src/lectures/exercise/functions/functions.go
+++ src/lectures/exercise/functions/functions.go
@@ -19,6 +19,40 @@ package main
 
 import "fmt"
 
-func main() {
+func greeting(name string) {
+	fmt.Println("Hello", name)
+}
 
+func hiThere() string {
+	return "Hi there!"
+}
+
+func sumThreeNumbers(num1, num2, num3 int) int {
+	return num1 + num2 + num3
+}
+
+func returnAnyNumber() int {
+	return 6
+}
+
+func returnAnyTwoNumbers() (int, int) {
+	return 3, 5
+}
+
+func main() {
+	greeting("Eric")
+
+	fmt.Println(hiThere())
+
+	fmt.Println("The total of three numbers are", sumThreeNumbers(7, 2, 1))
+
+	anyNumber := returnAnyNumber()
+	fmt.Println("Number from returnAnyNumber function is", anyNumber)
+
+	firstNumber, secondNumber := returnAnyTwoNumbers()
+	fmt.Println("Any two numbers are", firstNumber, "and", secondNumber)
+
+	a, b := returnAnyTwoNumbers()
+	calculateThreeNumbers := sumThreeNumbers(a, b, returnAnyNumber())
+	fmt.Println("The calculation betweet three number is", calculateThreeNumbers)
 }
